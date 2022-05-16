23392ebf8e5f60fadbd213b20ea09c32241ab7b4
Bimal Gharti Magar <bimal@endpointdev.com>
2022-05-16 18:55:41 +0545

    Separate line items and line item to components

---
+++ src/components/LineItem.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { computed, ref } from "@vue/reactivity";
+import { watch } from "vue";
+import CustomInput from "./CustomInput.vue";
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+  index: {
+    type: Number,
+    required: true,
+  },
+  hideClose: Boolean,
+});
+const emits = defineEmits(["update-item", "close"]);
+const lineItem = ref(props.item);
+const amount = computed(() => lineItem.value.rate * lineItem.value.quantity);
+
+watch(lineItem, () => {
+  emits("update-item", lineItem, props.index);
+});
+const removeItem = () => {
+  emits("close", props.index);
+};
+</script>
+<template>
+  <div class="flex">
+    <div class="m-0.5 flex-auto w-52">
+      <CustomInput
+        v-model="lineItem.description"
+        type="text"
+        label="Description"
+      />
+    </div>
+    <div class="m-0.5 flex-none w-32">
+      <CustomInput v-model="lineItem.quantity" />
+    </div>
+    <div class="m-0.5 flex-none w-32">
+      <CustomInput v-model="lineItem.rate" currency />
+    </div>
+    <div class="m-0.5 flex-none w-32 flex relative">
+      <CustomInput
+        v-model="amount"
+        currency
+        :is-used="!hideClose"
+        disabled
+        @close="removeItem"
+      />
+    </div>
+  </div>
+</template>
--- src/components/LineItems.vue
+++ src/components/LineItems.vue
@@ -1,51 +1,36 @@
-<script setup>
-import { computed, ref } from "@vue/reactivity";
-import { watch } from "vue";
-import CustomInput from "./CustomInput.vue";
-const props = defineProps({
-  item: {
-    type: Object,
-    required: true,
-  },
-  index: {
-    type: Number,
-    required: true,
-  },
-});
-const emits = defineEmits(["update-item", "close"]);
-const lineItem = ref(props.item);
-const amount = computed(() => lineItem.value.rate * lineItem.value.quantity);
-
-watch(lineItem, () => {
-  emits("update-item", lineItem, props.index);
-});
-const removeItem = () => {
-  emits("close", props.index);
-};
-</script>
 <template>
-  <div class="flex">
-    <div class="m-0.5 flex-auto w-52">
-      <CustomInput
-        v-model="lineItem.description"
-        input-type="text"
-        label="Description"
+  <div class="bg-slate-50 mx-auto h-min w-8/12">
+    <div class="flex bg-slate-200 rounded">
+      <div class="p-0.5 m-0.5 flex-auto w-52">Description</div>
+      <div class="p-0.5 m-0.5 flex-none w-32">Quantity</div>
+      <div class="p-0.5 m-0.5 flex-none w-32">Rate</div>
+      <div class="p-0.5 m-0.5 flex-none w-32">Amount</div>
+    </div>
+    <template v-for="(item, index) in items" :key="item.id">
+      <LineItem
+        :item="item"
+        :index="index"
+        :hide-close="index === 0 && items.length <= 1"
+        @update-item="$emit('udpate-item')"
+        @close="$emit('close')"
       />
-    </div>
-    <div class="m-0.5 flex-none w-32">
-      <CustomInput v-model="lineItem.quantity" />
-    </div>
-    <div class="m-0.5 flex-none w-32">
-      <CustomInput v-model="lineItem.rate" currency />
-    </div>
-    <div class="m-0.5 flex-none w-32 flex relative">
-      <CustomInput
-        v-model="amount"
-        currency
-        is-used
-        disabled
-        @close="removeItem"
-      />
-    </div>
+    </template>
+
+    <CustomButton class="mt-1 float-left ml-1" @click="$emit('add-item')">
+      Add Item
+    </CustomButton>
   </div>
 </template>
+
+<script setup>
+import LineItem from "./LineItem.vue";
+import CustomButton from "./CustomButton.vue";
+
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+defineEmits(["udpate-item", "close", "add-item"]);
+</script>
